ac31c1ddf4ab4cdf170b852cb6ca02301c0c2156
段枫 <128354383+laogou717@users.noreply.github.com>
2024-02-06 16:54:49 +0800

    Update page.tsx

--- src/app/(app)/thinking/page.tsx
+++ src/app/(app)/thinking/page.tsx
@@ -41,7 +41,7 @@ export default function Page() {
     <div>
       <header className="prose">
         <h1>思考</h1>
-        <h3>谢谢你听我诉说</h3>
+        <h3>一个普通人浅薄的思想💭</h3>
       </header>
 
       <main className="mt-10">
